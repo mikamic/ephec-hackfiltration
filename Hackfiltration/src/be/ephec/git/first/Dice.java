261868def358f271cae742c9c4408c70246aa2fb
mikamic <mika_mic@hotmail.com>
2015-12-03 19:57:16 +0100

    Création de l'objet Dice

---
+++ Hackfiltration/src/be/ephec/git/first/Dice.java
@@ -0,0 +1,39 @@
+package be.ephec.git.first;
+
+public class Dice {
+
+	private int chosenSide;
+	
+	public Dice() {
+		this.chosenSide = throwOfTheDice();
+	}
+	
+	/**
+	 * 
+	 * @return integer between 0 and 6
+	 */
+	public int throwOfTheDice() {
+		int random = (int)(Math.random()*100);
+		System.out.println(random);
+		if(random >= 0 && random < 100/6)
+			return 1;
+		else if(random >= 100/6 && random < 2*100/6)
+			return 2;
+		else if(random >= 2*100/6 && random < 3*100/6)
+			return 3;
+		else if(random >= 3*100/6 && random < 4*100/6)
+			return 4;
+		else if(random >= 4*100/6 && random < 5*100/6)
+			return 5;
+		else if(random >= 5*100/6 && random < 100)
+			return 6;
+		else
+			return 0;
+	}
+	
+	//de quoi tester ça...
+	public static void main(String[] args){
+		Dice dice = new Dice();
+		System.out.println(dice.chosenSide);
+	}
+}
