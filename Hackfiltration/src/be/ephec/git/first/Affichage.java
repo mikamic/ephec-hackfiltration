db0471ccfbff4c7780faf142bed88c04890b90fb
Valandinos <valandinos@hotmail.com>
2015-12-08 15:11:31 +0100

    Affichage MaJ
    Il est normal que les boutons soient alignés sur la gauche, il manque l'image qui remplir l'espace vide.

--- Hackfiltration/src/be/ephec/git/first/Affichage.java
+++ Hackfiltration/src/be/ephec/git/first/Affichage.java
@@ -1,10 +1,19 @@
-package Interface;
+package be.ephec.gui;
 
 import java.awt.EventQueue;
 
-public class Affichage {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JButton;
 
-	private JFrame frame;
+public class Affichage extends JFrame {
+
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -13,8 +22,8 @@ public class Affichage {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Affichage window = new Affichage();
-					window.frame.setVisible(true);
+					Affichage frame = new Affichage();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -23,49 +32,42 @@ public class Affichage {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public Affichage() {
-		initialize();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel diceResult = new JLabel("R\u00E9sultat du jet de d\u00E9s");
+		GridBagConstraints gbc_diceResult = new GridBagConstraints();
+		gbc_diceResult.fill = GridBagConstraints.HORIZONTAL;
+		gbc_diceResult.insets = new Insets(0, 0, 5, 5);
+		gbc_diceResult.gridx = 12;
+		gbc_diceResult.gridy = 0;
+		contentPane.add(diceResult, gbc_diceResult);
+		
+		JLabel floorLevel = new JLabel("Etage actuel");
+		GridBagConstraints gbc_floorLevel = new GridBagConstraints();
+		gbc_floorLevel.insets = new Insets(0, 0, 5, 5);
+		gbc_floorLevel.gridx = 12;
+		gbc_floorLevel.gridy = 1;
+		contentPane.add(floorLevel, gbc_floorLevel);
+		
+		JButton throwDice = new JButton("Lancer le d\u00E9");
+		GridBagConstraints gbc_throwDice = new GridBagConstraints();
+		gbc_throwDice.insets = new Insets(0, 0, 0, 5);
+		gbc_throwDice.gridx = 12;
+		gbc_throwDice.gridy = 2;
+		contentPane.add(throwDice, gbc_throwDice);
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{307, 143, 0};
-		gridBagLayout.rowHeights = new int[]{15, 15, 25, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		frame.getContentPane().setLayout(gridBagLayout);
-		
-		JLabel floor = new JLabel("Etage");
-		GridBagConstraints gbc_floor = new GridBagConstraints();
-		gbc_floor.anchor = GridBagConstraints.NORTH;
-		gbc_floor.fill = GridBagConstraints.HORIZONTAL;
-		gbc_floor.insets = new Insets(0, 0, 5, 0);
-		gbc_floor.gridx = 1;
-		gbc_floor.gridy = 0;
-		frame.getContentPane().add(floor, gbc_floor);
-		
-		JLabel diceResult = new JLabel("Res. Jet de dé");
-		GridBagConstraints gbc_diceResult = new GridBagConstraints();
-		gbc_diceResult.anchor = GridBagConstraints.NORTH;
-		gbc_diceResult.insets = new Insets(0, 0, 5, 0);
-		gbc_diceResult.gridx = 1;
-		gbc_diceResult.gridy = 1;
-		frame.getContentPane().add(diceResult, gbc_diceResult);
-		
-		JButton btnLancerLeD = new JButton("Lancer le dé");
-		GridBagConstraints gbc_btnLancerLeD = new GridBagConstraints();
-		gbc_btnLancerLeD.anchor = GridBagConstraints.NORTH;
-		gbc_btnLancerLeD.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnLancerLeD.gridx = 1;
-		gbc_btnLancerLeD.gridy = 2;
-		frame.getContentPane().add(btnLancerLeD, gbc_btnLancerLeD);
-	}
 }
