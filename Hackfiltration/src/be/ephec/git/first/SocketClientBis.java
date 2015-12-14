c5838ee5fa15c3bd89df8be6f61b951bdccd001d
Valandinos <valandinos@hotmail.com>
2015-12-14 01:42:16 +0100

    Socket client, OK pour utilisation en l'état

---
+++ Hackfiltration/src/be/ephec/git/first/SocketClientBis.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+import java.net.*;
+import java.io.*;
+
+public class SocketClientBis {
+	public static void main(String[] args) throws IOException {
+		String serverAddress = JOptionPane.showInputDialog("Adresse du Serveur SVP: ");
+		Socket socket = new Socket(serverAddress, 8080);
+		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+		output.writeObject("Coucou, voici un message du client !");
+		socket.close();
+		System.exit(0);
+	}
+
+}
