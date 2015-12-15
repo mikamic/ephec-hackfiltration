ef077e20462cd2490222d141207af6b19f061025
mikamic <mika_mic@hotmail.com>
2015-12-15 13:35:05 +0100

    Update Artefact.java
    int bonus -> private

--- Hackfiltration/src/be/ephec/git/first/Artefact.java
+++ Hackfiltration/src/be/ephec/git/first/Artefact.java
@@ -2,7 +2,7 @@ package be.ephec.git.first;
 
 public abstract class Artefact {
 
-	int bonus; //nbre de case que le pion peut passer
+	private int bonus; //nbre de case que le pion peut passer
 	
 	public Artefact() {
 		
@@ -15,11 +15,11 @@ public abstract class Artefact {
 	
 	public void moveForward(int bonus) {
 		Player pl = new Player();
-		//System.out.println("vous êtes à la case " + pl.getSquare());
+		//System.out.println("vous Ãªtes Ã  la case " + pl.getSquare());
 		pl.setSquare(pl.getSquare() + bonus);
 	}
 	
 	public void displayBonusMessage(String artefact, int number) {
-		System.out.println("Quelle chance, vous êtes tombé sur " + artefact + "!\nAvancez de " + number + " cases.");
+		System.out.println("Quelle chance, vous Ãªtes tombÃ© sur " + artefact + "!\nAvancez de " + number + " cases.");
 	}
 }
